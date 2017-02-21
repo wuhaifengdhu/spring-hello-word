1a6ed623e1f6aac28033bdb074343fbaa0c52a27
Wu Haifeng <hfwu@DMAL-admins-MacBook-Pro.local>
2017-02-21 17:03:36 +0800

    added gitignore file and removed unused files

--- target/itcm/resources/js/constants.js
+++
@@ -1,9 +0,0 @@
-function getSalt() {
-    return "$2a$11$o8vgy4olY7wcraHQKm4sqO";
-}
-
-Array.prototype.remove = function(from, to) {
-	var rest = this.slice((to || from) + 1 || this.length);
-	this.length = from < 0 ? this.length + from : from;
-	return this.push.apply(this, rest);
-};
